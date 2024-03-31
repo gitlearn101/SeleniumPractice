ca00047e93248c30aa5d2a1d424f9a3c5c091bbd
gitlearn101 <anand.learn101@gmail.com>
2024-03-31 15:47:49 +0530

    Day19 committed

--- src/seleniumcoding50dayschallenge/Day19_Emoji.java
+++ src/seleniumcoding50dayschallenge/Day19_Emoji.java
@@ -7,24 +7,29 @@
 2)Click on the set of each available rate value and assert by image, text, and number.
 4)Verify that the Emoji image, the text message e.g-"I don't like it" and the number out of 5 e.g- "2 out of 5". Verify for all the 5 ratings. */
 
-
 package seleniumcoding50dayschallenge;
 
-import java.io.IOException;
 import java.time.Duration;
+import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Day19_Emoji {
-	
-public static WebDriver driver;
-	
+
+	public static WebDriver driver;
+	public static int input = 5;
 
 	@BeforeTest
 	void setup() {
@@ -42,24 +47,83 @@ public static WebDriver driver;
 
 	}
 
-	//@AfterTest
+	// @AfterTest
 	void teardown() {
 		driver.quit();
 	}
 
-	
 	@Test
-	void testOne() throws InterruptedException, IOException {
+	public void EmojiRating() throws InterruptedException {
 		
 		driver.get("https://qaplayground.dev/apps/rating/");
-		
-		
-		
-		
-		
-	}
-		
 
-	
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+		List<WebElement> starsRating = driver
+				.findElements(By.xpath("//label[contains(@class,'star-') and contains(@class,'fa-star')]"));
+		// Click on stars based on user input using a simple loop
+		for (int i = 0; i < Math.min(input, starsRating.size()); i++) {
+			Thread.sleep(1000);
+			starsRating.get(i).click();
+		}
+
+		switch (input) {
+		case 1:
+			WebElement star1_emoji = wait.until(ExpectedConditions
+					.visibilityOfElementLocated(By.cssSelector("img[src='emojis/emoji-" + input + ".png']")));
+			Assert.assertTrue(star1_emoji.isDisplayed());
+			Assert.assertEquals(fetchPsuedoElement_Comment(), "I just hate it");
+			Assert.assertEquals(fetchPsuedoElement_Numb(), "1 out of 5");
+			System.out.println("Test is passed..");
+			break;
+		case 2:
+			WebElement star2_emoji = wait.until(ExpectedConditions
+					.visibilityOfElementLocated(By.cssSelector("img[src='emojis/emoji-" + input + ".png']")));
+			Assert.assertTrue(star2_emoji.isDisplayed());
+			Assert.assertEquals(fetchPsuedoElement_Comment(), "I don't like it");
+			Assert.assertEquals(fetchPsuedoElement_Numb(), "2 out of 5");
+			System.out.println("Test is passed..");
+			break;
+		case 3:
+			WebElement star3_emoji = wait.until(ExpectedConditions
+					.visibilityOfElementLocated(By.cssSelector("img[src='emojis/emoji-" + input + ".png']")));
+			Assert.assertTrue(star3_emoji.isDisplayed());
+			Assert.assertEquals(fetchPsuedoElement_Comment(), "This is awesome");
+			Assert.assertEquals(fetchPsuedoElement_Numb(), "3 out of 5");
+			System.out.println("Test is passed..");
+			break;
+		case 4:
+			WebElement star4_emoji = wait.until(ExpectedConditions
+					.visibilityOfElementLocated(By.cssSelector("img[src='emojis/emoji-" + input + ".png']")));
+			Assert.assertTrue(star4_emoji.isDisplayed());
+			Assert.assertEquals(fetchPsuedoElement_Comment(), "I just like it");
+			Assert.assertEquals(fetchPsuedoElement_Numb(), "4 out of 5");
+			System.out.println("Test is passed..");
+			break;
+		case 5:
+			WebElement star5_emoji = wait.until(ExpectedConditions
+					.visibilityOfElementLocated(By.cssSelector("img[src='emojis/emoji-" + input + ".png']")));
+			Assert.assertTrue(star5_emoji.isDisplayed());
+			Assert.assertEquals(fetchPsuedoElement_Comment(), "I just love it");
+			Assert.assertEquals(fetchPsuedoElement_Numb(), "5 out of 5");
+			System.out.println("Test is passed..");
+			break;
+		default:
+			System.out.println("Something went wrong...");
+		}
+
+	}
+
+	public static String fetchPsuedoElement_Comment() {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		String script = "return window.getComputedStyle(document.querySelector('span.text'),'::before').getPropertyValue('content').replace(/\"/g, '')";
+		return (String) js.executeScript(script);
+	}
+
+	public static String fetchPsuedoElement_Numb() {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		String script = "return window.getComputedStyle(document.querySelector('span.numb'), '::before').getPropertyValue('content').replace(/\"/g, '')";
+		return (String) js.executeScript(script);
+	}
 
 }
